b1c112d106c7af96f4ebb0e120f0526d845dd0d5
liuzhanliang <zhanliangliu@qq.com>
2018-05-04 07:57:26 +0800

    Update proxy_table.go

--- proxy_table.go
+++ proxy_table.go
@@ -2,19 +2,42 @@ package proxypool
 
 import (
 	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/liuzl/sqlext"
 )
 
 func ReadProxy() ([]*Proxy, error) {
-	db := GetMySQLHandler()
 	var records []*Proxy
-	ret := db.Where(
+	ret := GetMySQLHandler().Where(
 		"update_time>last_fail_time OR total_crawl<5 OR total_fail/total_crawl<0.5").
 		Find(&records)
 	fmt.Printf("%+v\n", ret)
 	return records, nil
 }
 
-func InsertProxy(proxyList []*Proxy) error {
-	// TODO https://github.com/btfak/sqlext
+func InsertProxy(proxies []*Proxy) error {
+	if len(proxies) <= 0 {
+		return nil
+	}
+	db := GetMySQLHandler()
+	_, err := sqlext.BatchInsert(db.DB(), proxies)
+	return err
+}
+
+func InvalidProxy(addr string) error {
+	GetMySQLHandler().Model(Proxy{}).Where("IpPort=?", addr).Updates(
+		map[string]interface{}{
+			"last_fail_time": gorm.Expr("now()"),
+			"total_fail":     gorm.Expr("total_fail+?", 1),
+		})
 	return nil
+}
+
+func AcquireProxy(addr string) error {
+	GetMySQLHandler().Model(Proxy{}).Where("IpPort=?", addr).Updates(
+		map[string]interface{}{
+			"total_crawl": gorm.Expr("total_crawl+?", 1),
+		})
+	return nil
+
 }
