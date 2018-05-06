380781ec1bab0546606f4430a44674b521c29ae7
liuzhanliang <zhanliangliu@qq.com>
2018-05-06 18:38:03 +0800

    Add func for validating proxies

--- update_proxy.go
+++ update_proxy.go
@@ -2,6 +2,8 @@ package proxypool
 
 import (
 	"github.com/golang/glog"
+	"github.com/liuzl/dl"
+	"strings"
 )
 
 var NameFuncs = make(map[string]func() []string)
@@ -13,4 +15,18 @@ func CrawlProxy() {
 		ret := f()
 		glog.Info(ret)
 	}
+}
+
+func Validate(addr string) bool {
+	req := &dl.HttpRequest{
+		Url:      "https://www.baidu.com/",
+		UseProxy: true,
+		Proxy:    "http://" + addr,
+	}
+	resp := dl.Download(req)
+	if resp.Error != nil {
+		glog.Error(resp.Error)
+		return false
+	}
+	return strings.Contains(resp.Text, "<title>百度一下，你就知道</title>")
 }
