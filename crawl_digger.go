a95864050e1ae2e276418c85d8388a3b15fd07dd
liuzhanliang <zhanliangliu@qq.com>
2018-05-06 18:55:50 +0800

    seperate files for proxy sites

--- crawl_digger.go
+++ crawl_digger.go
@@ -10,36 +10,16 @@ import (
 )
 
 func init() {
-	NameFuncs["cz88"] = crawlCZ88
 	NameFuncs["digger"] = crawlDigger
 }
 
 var (
-	Cz88Url     = "http://www.cz88.net/proxy/index.shtml"
-	Cz88Pattern = `<div class="ip">.*?([\d\.]*?)</div><div class="port">([\d]*?)</div>`
-	ReCz88      = regexp.MustCompile(Cz88Pattern)
-
 	DiggerUrl        = "http://www.site-digger.com/html/articles/20110516/proxieslist.html"
 	DiggerPattern    = `<td><script>document.write\(decrypt\("(.*)"\)\);</script></td>`
 	ReDigger         = regexp.MustCompile(DiggerPattern)
 	DiggerKeyPattern = `var baidu_union_id = "(.+)";`
 	ReDiggerKey      = regexp.MustCompile(DiggerKeyPattern)
 )
-
-func crawlCZ88() []string {
-	glog.Infof("get proxies from: %s", Cz88Url)
-	resp := dl.DownloadUrl(Cz88Url)
-	if resp.Error != nil {
-		glog.Error(resp.Error)
-		return nil
-	}
-	matches := ReCz88.FindAllStringSubmatch(resp.Text, -1)
-	var ret []string
-	for _, m := range matches {
-		ret = append(ret, fmt.Sprintf("%s:%s", m[1], m[2]))
-	}
-	return ret
-}
 
 func crawlDigger() []string {
 	glog.Infof("get proxies from: %s", DiggerUrl)
