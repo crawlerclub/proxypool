8742b4e537ad73650dda8d7fb44d20e163b6b942
liuzhanliang <zhanliangliu@qq.com>
2018-05-07 11:18:41 +0800

    proxypool cmd done

--- proxypool/main.go
+++ proxypool/main.go
@@ -18,6 +18,7 @@ func stop(sigs chan os.Signal, exitCh chan bool) {
 
 func main() {
 	flag.Parse()
+
 	exitCh := make(chan bool)
 	sigs := make(chan os.Signal)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
@@ -26,6 +27,13 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go proxypool.Run(exitCh, &wg)
+
+	server := &proxypool.ProxyServer{}
+	wg.Add(1)
+	go server.Run(exitCh, &wg)
+
+	go server.Web()
+
 	wg.Wait()
 	glog.Info("exit!")
 }
