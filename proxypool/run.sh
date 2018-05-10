632df753d2274c0daf2845e14a80530e9885b02e
liuzhanliang <zhanliangliu@qq.com>
2018-05-10 12:01:23 +0800

    turn on update flag

--- proxypool/run.sh
+++ proxypool/run.sh
@@ -1,1 +1,1 @@
-./proxypool -logtostderr -mysql_uri "root:he110@/proxy?charset=utf8&parseTime=True&loc=Local" 
+./proxypool -logtostderr -mysql_uri "root:he110@/proxy?charset=utf8&parseTime=True&loc=Local" -update
