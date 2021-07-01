d08ba5ee32188f4983ed5aaa152d943ba22a3058
liuzl <liang@zliu.org>
2021-07-02 07:45:00 +0800

    update docker command

--- mysql_docker.sh
+++ mysql_docker.sh
@@ -1,2 +1,2 @@
-DIR="/Users/hwang/proxypool/mysql"
-docker run --name first -p 3306:3306 -v $DIR/data:/var/lib/mysql -v $DIR/mysql-files:/var/lib/mysql-files -e MYSQL\_ROOT\_PASSWORD=he110 -d mysql:5.6
+DIR="/data/zliu/proxypool/mysql"
+docker run --name first -u 1000:1000 -p 3306:3306 -v $DIR/data:/var/lib/mysql -v $DIR/mysql-files:/var/lib/mysql-files -e MYSQL\_ROOT\_PASSWORD=he110 -d mysql:5.6
